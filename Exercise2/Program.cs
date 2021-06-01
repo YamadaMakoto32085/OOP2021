ea78b28cfafd939274a6e599e81928be6fe56171
山田 真人 <32085@ojs.ac.jp>
2021-06-01 10:25:00 +0900

    P79 問題2.2 完成

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -7,7 +7,27 @@ using System.Threading.Tasks;
 namespace Exercise2 {
     class Program {
         static void Main(string[] args) {
-
+            if (args.Length >= 1 && args[0] == "-toi") {
+                //メートルからインチへの対応表を出力
+                PrintMeterToInchList(1, 10);
+            } else {
+                //インチからメートルへの対応表を出力
+                PrintInchToMeterList(1, 10);
+            }
+        }
+        //メートルからインチへの対応表を出力
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} inch", meter, inch);
+            }
+        }
+        //インチからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
         }
     }
 }
