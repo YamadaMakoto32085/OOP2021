e74160faa15b2a4c08ada269c85f7405cdb9e222
山田 真人 <32085@ojs.ac.jp>
2021-06-01 09:31:57 +0900

    P79 問題2.1 完成

--- Exercise1/Song.cs
+++ Exercise1/Song.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Song {
+        //2.1.1
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        //2.1.2
+        public Song(string title,string artistname,int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
     }
 }
