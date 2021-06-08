00c91e37669cbc79cb66d567efcd962647e562e2
山田 真人 <32085@ojs.ac.jp>
2021-06-08 11:53:12 +0900

    p125 リスト4.41

--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            int count = 0;
+
+            Console.WriteLine($"後置:{count++}");
+            
+            Console.WriteLine($"前置:{count++}");
+
         }
+
+        private static Product GetProductA() {
+            Sale sale = new Sale();
+            
+            return sale?.Product;  //null条件演算子
+        }
+    }
+    class Sale {
+        public string ShopName { get; set; } = "abcde";
+        public int Amount { get; set; } = 12340;
+        public Product Product { get; set; }
     }
 }
