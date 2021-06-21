57e3a56d5bc1b487dfc55df336058eb9faed225b
山田 真人 <32085@ojs.ac.jp>
2021-06-21 14:17:39 +0900

    設問1 完成

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,36 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            var scores = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                var score = new Student { //Studentメソッドを作成
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(score);
+            }
+            return scores;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
-            
+            var dict = new Dictionary<string, int>();
+            foreach(var score in _score) {
+                if (dict.ContainsKey(score.Name))
+                    dict[score.Name] += score.Score;
+                else
+                    dict[score.Name] = score.Score;
+            }
+            return dict;
         }
     }
 }
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,7 @@
 ﻿namespace Test01 {
     class Student {
-
-
-
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
     }
 }
