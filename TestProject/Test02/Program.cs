14c4a38648472fb1c3fc3bfa60e012d29fdf9da0
山田 真人 <32085@ojs.ac.jp>
2021-06-21 14:29:38 +0900

    設問2 完成

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -36,17 +36,15 @@ namespace Test02 {
         //問題１　合計値を表示
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
-
-
-
+            var query = numbers.Sum();
+            Console.WriteLine(query);
         }
 
         //問題２　偶数の最大値を表示
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
-
-
-
+            var query = numbers.Max(i => i % 2 == 0);
+            Console.WriteLine(query);
         }
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
@@ -59,9 +57,9 @@ namespace Test02 {
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
-
-
-
+            var query = numbers.Where(i => 10 <= i && i <=50);
+            foreach (int i in query)
+                Console.WriteLine(i);
         }
     }
 }
