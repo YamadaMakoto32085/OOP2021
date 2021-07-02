a324fb663e36320264598812a953afb58d992e72
山田 真人 <32085@ojs.ac.jp>
2021-07-02 14:36:57 +0900

    p203 問題7.1 完成

---
+++ Chapter07/Exercise1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+        }
+
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<Char, int>();
+            foreach (var c in text) {
+                //大文字小文字を区別しないためすべて大文字に変換して処理
+                var uc = char.ToUpper(c); 
+                if ('A' <= uc && uc <= 'Z') {
+                    if (dict.ContainsKey(uc)) {
+                        dict[uc]++;
+                    } else {
+                        dict[uc] = 1;
+                    }
+                }
+            }
+            //Aから並べ替えて出力
+            foreach (var item in dict.OrderBy(d => d.Key)) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+        }
+
+        private static void Exercise1_2(string text) {
+            var dict = new SortedDictionary<Char, int>();
+            foreach (var c in text) {
+                //大文字小文字を区別しないためすべて大文字に変換して処理
+                var uc = char.ToUpper(c);
+                if ('A' <= uc && uc <= 'Z') {
+                    if (dict.ContainsKey(uc)) {
+                        dict[uc]++;
+                    } else {
+                        dict[uc] = 1;
+                    }
+                }
+            }
+            //Aから並べ替えて出力
+            foreach (var item in dict) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+        }
+    }
+}
