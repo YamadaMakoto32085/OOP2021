a960b6808dd066b586881828270cd4263bfae2e1
真人 山田 <32085@ojs.ac.jp>
2021-08-23 15:51:12 +0900

    夏休み明け

---
+++ Sample0823/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void Exec_Click(object sender, EventArgs e){
+            int num1 = int.Parse(Value.Text); //整数に変換
+            int num2 = int.Parse(Jyou.Text);
+            
+        }
+    }
+}
