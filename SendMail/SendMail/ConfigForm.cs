b29db4e5fb3b2d3d2278f1447c2b91609031a187
真人 山田 <32085@ojs.ac.jp>
2021-10-06 15:28:28 +0900

    XML保存 完成

--- SendMail/SendMail/ConfigForm.cs
+++ SendMail/SendMail/ConfigForm.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SendMail
 {
@@ -34,6 +36,13 @@ namespace SendMail
             SettingRegist();
             this.Close();
         }
+
+        //適用ボタン
+        private void btApply_Click(object sender, EventArgs e)
+        {
+            SettingRegist();//送信データ登録
+        }
+
         //送信データ登録
         private void SettingRegist()
         {
@@ -42,16 +51,24 @@ namespace SendMail
             settings.MailAddr = tbUserName.Text;
             settings.Pass = tbPass.Text;
             settings.Ssl = cbSsl.Checked;
+
+            var xws = new XmlWriterSettings
+            {
+                Encoding = new System.Text.UTF8Encoding(false),
+                Indent = true,
+                IndentChars = "   ",
+            };
+
+            using (var writer = XmlWriter.Create("mailsetting.xml", xws))
+            {
+                var serializer = new DataContractSerializer(settings.GetType());
+                serializer.WriteObject(writer, settings);
+            }
         }
 
         private void btCancel_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-        //適用ボタン
-        private void btApply_Click(object sender, EventArgs e)
-        {
-            SettingRegist();//送信データ登録
         }
     }
 }
