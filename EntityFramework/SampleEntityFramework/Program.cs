ceb856f4e176f1b837b9e736a36f57f94477093c
真人 山田 <32085@ojs.ac.jp>
2021-10-21 10:40:07 +0900

    演習問題完成

--- EntityFramework/SampleEntityFramework/Program.cs
+++ EntityFramework/SampleEntityFramework/Program.cs
@@ -34,6 +34,7 @@ namespace SampleEntityFramework
         {
             using(var db = new BooksDbContext())
             {
+                InsertBooks();
                 var author1 = new Author
                 {
                     Birthday = new DateTime(1888, 12, 26),
@@ -48,13 +49,16 @@ namespace SampleEntityFramework
                     Name = "川端康成",
                 };
                 db.Authors.Add(author2);
-                db.SaveChanges();
-                var author3 = db.Authors.Single(a => a.Name == "夏目漱石");
                 var book1 = new Book
                 {
                     Title = "こころ",
                     PublishedYear = 1991,
-                    Author = author3,
+                    Author = new Author
+                    {
+                        Birthday = new DateTime(1867, 2, 9),
+                        Gender = "M",
+                        Name = "夏目漱石",
+                    }
                 };
                 db.Books.Add(book1);
                 var book2 = new Book
@@ -71,12 +75,16 @@ namespace SampleEntityFramework
                     Author = author1
                 };
                 db.Books.Add(book3);
-                var author4 = db.Authors.Single(a => a.Name == "宮沢賢治");
                 var book4 = new Book
                 {
                     Title = "注文の多い料理店",
                     PublishedYear = 2000,
-                    Author = author4,
+                    Author = new Author
+                    {
+                        Birthday = new DateTime(1896, 8, 27),
+                        Gender = "M",
+                        Name = "宮沢賢治",
+                    }
                 };
                 db.Books.Add(book4);
                 db.SaveChanges();
@@ -85,28 +93,59 @@ namespace SampleEntityFramework
 
         private static void Exercise13_1_2()
         {
-            using(var db = new BooksDbContext())
+            using (var db = new BooksDbContext())
             {
-                foreach (var books in db.Books)
+                foreach (var book in db.Books)
                 {
-
+                    Console.WriteLine("{0} {1} {2}({3:yyyy/MM/dd})",
+                                        book.Title, book.PublishedYear,
+                                        book.Author.Name, book.Author.Birthday);
                 }
             }
         }
 
         private static void Exercise13_1_3()
         {
-            
+            using(var db = new BooksDbContext())
+            {
+                var books = db.Books.Where(b => b.Title.Length == db.Books.Max(x => x.Title.Length));
+                foreach(var book in books)
+                {
+                    Console.WriteLine("{0} {1} {2}({3:yyyy/MM/dd}", book.Title, book.PublishedYear,
+                                                                   book.Author.Name, book.Author.Birthday);
+                }
+            }
         }
 
         private static void Exercise13_1_4()
         {
-            
+            using(var db = new BooksDbContext())
+            {
+                var books = db.Books.OrderBy(b => b.PublishedYear).Take(3);
+                foreach(var book in books)
+                {
+                    Console.WriteLine("{0} {1} {2}({3:yyyy/MM/dd}", book.Title, book.PublishedYear,
+                                                                   book.Author.Name, book.Author.Birthday);
+                }
+            }
         }
 
         private static void Exercise13_1_5()
         {
-            
+            using(var db = new BooksDbContext())
+            {
+                var authors = db.Authors.OrderByDescending(a => a.Birthday);
+                foreach (var author in authors)
+                {
+                    Console.WriteLine("{0} {1:yyyy/MM}", author.Name, author.Birthday);
+                    foreach (var book in author.Books)
+                    {
+                        Console.WriteLine("  {0} {1}", book.Title, book.PublishedYear,
+                                                      book.Author.Name, book.Author.Birthday);
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
 
         // List 13-12
